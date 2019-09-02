b09e9a63cc654a4e939211015856245695ebd1fe
Campbell Baron <campbelljbaron@gmail.com>
2019-09-02 13:44:26 -0400

    Update Contents.swift

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -3,24 +3,24 @@
 import UIKit
 
 //step 1: we are creating a dict for each player with all their personal information
-let joeSmith : [String : String] = ["Name" : "Joe Smith", "Height" : "42", "Expirence" : "YES", "Guardian" : "Jim and Jan Smith"]
-let jillTanner : [String : String] = ["Name" : "Jill Tanner", "Height" : "36", "Expirence" : "YES", "Guardian" : "Clara Tanner"]
-let billBon : [String : String] = ["Name" : "Bill Bon", "Height" : "43", "Expirence" : "YES", "Guardian" : "Sara and Jenny Bon"]
-let evaGordon : [String : String] = ["Name" : "Eva Gordon", "Height" : "45", "Expirence" : "NO", "Guardian" : "Wendy and Mike Gordon"]
-let mattGill : [String : String] = ["Name" : "Matt Gill", "Height" : "40", "Expirence" : "NO", "Guardian" : "Charles and Sylvia Gill"]
-let kimmyStein : [String : String] = ["Name" : "Kimmy Stein", "Height" : "41", "Expirence" : "NO", "Guardian" : "Bill and Hillary Stein"]
-let sammyAdams : [String : String] = ["Name" : "Sammy Adams", "Height" : "45", "Expirence" : "NO", "Guardian" : "Jeff Adams"]
-let karlSaygan : [String : String] = ["Name" : "Karl Saygan", "Height" : "42", "Expirence" : "YES", "Guardian" : "Heather Bledsoe"]
-let suzaneGreenberg : [String : String] = ["Name" : "Suzane Greenberg", "Height" : "44", "Expirence" : "YES", "Guardian" : "Henrietta Dumas"]
-let salDali : [String : String] = ["Name" : "Sal Dali", "Height" : "41", "Expirence" : "NO", "Guardian" : "Gala Dali"]
-let joeKavalier : [String : String] = ["Name" : "Joe Kavalier", "Height" : "39", "Expirence" : "NO", "Guardian" : "Sam and Elaine Kavalier"]
-let benFinkelstein : [String : String] = ["Name" : "Ben Finkelstein", "Height" : "44", "Expirence" : "NO", "Guardian" : "Aaron and Jill Finkelstein"]
-let diegoSoto : [String : String] = ["Name" : "Diego Soto", "Height" : "41", "Expirence" : "YES", "Guardian" : "Robin and Sarika Soto"]
-let chloeAlaska : [String : String] = ["Name" : "Chloe Alaska", "Height" : "47", "Expirence" : "NO", "Guardian" : "David and Jamie Alaska"]
-let arnoldWillis : [String : String] = ["Name" : "Arnold Willis", "Height" : "43", "Expirence" : "NO", "Guardian" : "Claire Willis"]
-let phillipHelm : [String : String] = ["Name" : "Phillip Helm", "Height" : "44", "Expirence" : "YES", "Guardian" : "Thomas Helm and Eva Jones"]
-let lesClay : [String : String] = ["Name" : "Les Clay", "Height" : "42", "Expirence" : "YES", "Guardian" : "Wynonna Brown"]
-let herschelKrustofski : [String : String] = ["Name" : "Herschel Krustofski", "Height" : "45", "Expirence" : "YES", "Guardian" : "Hyman and Rachel Krustofski"]
+let joeSmith : [String : String] = ["name" : "Joe Smith", "height" : "42", "isExperienced" : "YES", "guardian" : "Jim and Jan Smith"]
+let jillTanner : [String : String] = ["name" : "Jill Tanner", "height" : "36", "isExperienced" : "YES", "guardian" : "Clara Tanner"]
+let billBon : [String : String] = ["name" : "Bill Bon", "height" : "43", "isExperienced" : "YES", "guardian" : "Sara and Jenny Bon"]
+let evaGordon : [String : String] = ["name" : "Eva Gordon", "height" : "45", "isExperienced" : "NO", "guardian" : "Wendy and Mike Gordon"]
+let mattGill : [String : String] = ["name" : "Matt Gill", "height" : "40", "isExperienced" : "NO", "guardian" : "Charles and Sylvia Gill"]
+let kimmyStein : [String : String] = ["name" : "Kimmy Stein", "height" : "41", "isExperienced" : "NO", "guardian" : "Bill and Hillary Stein"]
+let sammyAdams : [String : String] = ["name" : "Sammy Adams", "height" : "45", "isExperienced" : "NO", "guardian" : "Jeff Adams"]
+let karlSaygan : [String : String] = ["name" : "Karl Saygan", "height" : "42", "isExperienced" : "YES", "guardian" : "Heather Bledsoe"]
+let suzaneGreenberg : [String : String] = ["name" : "Suzane Greenberg", "height" : "44", "isExperienced" : "YES", "guardian" : "Henrietta Dumas"]
+let salDali : [String : String] = ["name" : "Sal Dali", "height" : "41", "isExperienced" : "NO", "guardian" : "Gala Dali"]
+let joeKavalier : [String : String] = ["name" : "Joe Kavalier", "height" : "39", "isExperienced" : "NO", "guardian" : "Sam and Elaine Kavalier"]
+let benFinkelstein : [String : String] = ["name" : "Ben Finkelstein", "height" : "44", "isExperienced" : "NO", "guardian" : "Aaron and Jill Finkelstein"]
+let diegoSoto : [String : String] = ["name" : "Diego Soto", "height" : "41", "isExperienced" : "YES", "guardian" : "Robin and Sarika Soto"]
+let chloeAlaska : [String : String] = ["name" : "Chloe Alaska", "height" : "47", "isExperienced" : "NO", "guardian" : "David and Jamie Alaska"]
+let arnoldWillis : [String : String] = ["name" : "Arnold Willis", "height" : "43", "isExperienced" : "NO", "guardian" : "Claire Willis"]
+let phillipHelm : [String : String] = ["name" : "Phillip Helm", "height" : "44", "isExperienced" : "YES", "guardian" : "Thomas Helm and Eva Jones"]
+let lesClay : [String : String] = ["name" : "Les Clay", "height" : "42", "isExperienced" : "YES", "guardian" : "Wynonna Brown"]
+let herschelKrustofski : [String : String] = ["name" : "Herschel Krustofski", "height" : "45", "isExperienced" : "YES", "guardian" : "Hyman and Rachel Krustofski"]
 
 //step 2: creating empty arrays to hold various players in teams
 var teamSharks : [[String : String]] = []
@@ -46,9 +46,9 @@ var nonExpirencedPlayers : [[String : String]] = []
 func sortPlayers() {
     
     for player in players {
-        if player["Expirence"] == "YES" {
+        if player["isExperienced"] == "YES" {
             expirencedPlayers.append(player)
-        } else if player["Expirence"] == "NO" {
+        } else if player["isExperienced"] == "NO" {
             nonExpirencedPlayers.append(player)
         }
     }
@@ -65,34 +65,37 @@ func createTeams() {
     //expirenced players are added to teams
     
     //variable which will determine the number of players on each team
-    var teamNum = players.count / league.count
+    var teamNumber = players.count / league.count
     
-    for exPlayer in 0...expirencedPlayers.count {
+    //variable will determine how many expirenced players and non expirenced players on each team
+    var halfOfTeam = players.count / league.count / 2
+    
+    for expirencedPlayer in 0...expirencedPlayers.count {
         
-        if teamSharks.count != teamNum / 2 {
-            teamSharks.append(expirencedPlayers[exPlayer])
+        if teamSharks.count != halfOfTeam {
+            teamSharks.append(expirencedPlayers[expirencedPlayer])
             
-        } else if teamDragons.count != teamNum / 2 && teamSharks.count == teamNum / 2 {
-            teamDragons.append(expirencedPlayers[exPlayer])
+        } else if teamDragons.count != halfOfTeam && teamSharks.count == halfOfTeam {
+            teamDragons.append(expirencedPlayers[expirencedPlayer])
             
-        } else if teamRaptors.count != teamNum / 2 && teamSharks.count == teamNum / 2 && teamDragons.count == teamNum / 2 {
-            teamRaptors.append(expirencedPlayers[exPlayer])
+        } else if teamRaptors.count != halfOfTeam && teamSharks.count == halfOfTeam && teamDragons.count == halfOfTeam {
+            teamRaptors.append(expirencedPlayers[expirencedPlayer])
         }
         
     }
     
-    for nonExpPlater in 0...nonExpirencedPlayers.count {
+    for nonExpirencedPlayer in 0...nonExpirencedPlayers.count {
         
     //non-expirenced players are added to teams
 
-        if teamSharks.count != teamNum {
-            teamSharks.append(nonExpirencedPlayers[nonExpPlater])
+        if teamSharks.count != teamNumber {
+            teamSharks.append(nonExpirencedPlayers[nonExpirencedPlayer])
             
-        } else if teamDragons.count != teamNum && teamSharks.count == teamNum {
-            teamDragons.append(nonExpirencedPlayers[nonExpPlater])
+        } else if teamDragons.count != teamNumber && teamSharks.count == teamNumber {
+            teamDragons.append(nonExpirencedPlayers[nonExpirencedPlayer])
             
-        } else if teamRaptors.count != teamNum && teamSharks.count == teamNum && teamDragons.count == teamNum {
-            teamRaptors.append(nonExpirencedPlayers[nonExpPlater])
+        } else if teamRaptors.count != teamNumber && teamSharks.count == teamNumber && teamDragons.count == teamNumber {
+            teamRaptors.append(nonExpirencedPlayers[nonExpirencedPlayer])
         }
         
     }
@@ -108,23 +111,22 @@ func createLetters() {
     
     for raptors in teamRaptors {
         
-        letters.append("Greetings \(raptors["Guardian"] ?? ""), we would like to inform you that \(raptors["Name"] ?? "") has been placed on Team Raptors. Team Raptors has practices every \(raptorsPractice). We look forward to seeing you there!")
+        letters.append("Greetings \(raptors["guardian"] ?? ""), we would like to inform you that \(raptors["name"] ?? "") has been placed on Team Raptors. Team Raptors has practices every \(raptorsPractice). We look forward to seeing you there!")
     }
     
     for sharks in teamSharks {
         
-        letters.append("Greetings \(sharks["Guardian"] ?? ""), we would like to inform you that \(sharks["Name"] ?? "") has been placed on Team Sharks. Team Sharks has practices every \(sharksPractice). We look forward to seeing you there!")
+        letters.append("Greetings \(sharks["guardian"] ?? ""), we would like to inform you that \(sharks["name"] ?? "") has been placed on Team Sharks. Team Sharks has practices every \(sharksPractice). We look forward to seeing you there!")
     }
     
     for dragons in teamDragons {
         
-        letters.append("Greetings \(dragons["Guardian"] ?? ""), we would like to inform you that \(dragons["Name"] ?? "") has been placed on Team Dragons. Team Dragons has practices every \(dragonsPractice). We look forward to seeing you there!")
+        letters.append("Greetings \(dragons["guardian"] ?? ""), we would like to inform you that \(dragons["name"] ?? "") has been placed on Team Dragons. Team Dragons has practices every \(dragonsPractice). We look forward to seeing you there!")
     }
     
     //all the letters are stored into an empty array and then printed
     for letter in letters {
-        print(letter)
-        print("                                                                                                                      ")
+        print(letter, separator: "", terminator: "\n\n")
     }
     
 }
